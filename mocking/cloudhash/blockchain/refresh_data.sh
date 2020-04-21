56c3b2d8adf6c4b30e94728ea1b5cf8afda78e1e
Frank Meus <frank.meus@gmail.com>
2020-04-21 10:48:49 +0200

    Updated blockchain info

--- mocking/cloudhash/blockchain/refresh_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-array=(getdifficulty getblockcount avgtxnumber interval unconfirmedcount 24hrtransactioncount hashrate totalbc bcperblock)
-
-## now loop through the above array
-for i in "${array[@]}"
-do
-	curl -s https://blockchain.info/q/$i -o $i
-done
